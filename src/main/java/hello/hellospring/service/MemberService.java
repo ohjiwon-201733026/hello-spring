74770ae2e194bbd20f65feb1b8ef750f4386d936
OH JIWON <ojw970725@gmail.com>
2021-08-30 08:21:16 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // Dependency Injection
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
+    }
+
+    /*
+    join
+     */
+    public Long join(Member member){
+        validateDuplicate(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicate(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    /*
+    findAll
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
