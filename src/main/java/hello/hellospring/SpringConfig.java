d3b7dd8f28ea2a7e22a5fa6b1f99bae8651d2231
OH JIWON <ojw970725@gmail.com>
2021-08-31 08:55:07 +0900

    JdbcTemplateMemberRepository

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -7,27 +7,23 @@ import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+import javax.swing.text.html.parser.Entity;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource){
-        this.dataSource=dataSource;
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRespository(dataSource);
-        return new JdbcMemberRespository(dataSource);
 
-    }
 }
